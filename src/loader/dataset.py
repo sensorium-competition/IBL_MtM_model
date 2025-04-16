45b3084d343ba10bb986a0ff7f5daa177c2b16d7
Polina Turishcheva <pturishcheva@gmail.com>
2025-04-16 16:15:29 +0200

    works with test scans now

--- src/loader/dataset.py
+++ src/loader/dataset.py
@@ -5,11 +5,15 @@ from experanto.configs import DEFAULT_CONFIG as cfg
 
 
 def build_dataloader(config, inference=False):
-
+    print(f'with train_loader')
     # Data paths.
-    path_pre = config['data']['path']
-    full_paths = [os.path.join(path_pre, f) for f in config['data']['scans']]
-
+    path_pre = config['data']['path1']
+    full_paths = [os.path.join(path_pre, f) for f in config['data']['scans1']]
+    if 'path2' in config['data']:
+        full_paths = full_paths + [os.path.join(config['data']['path2'], f) for f in config['data']['scans2']]
+    print('Full paths:')
+    print(full_paths)
+    print('\n')
     # Overwrite default config with custom values.
     # Overwrite default config with custom values.
     if 'screen_chunk_size' in config['data']:
@@ -66,15 +70,22 @@ def build_dataloader(config, inference=False):
     cfg.dataloader.num_workers = config['data']['num_workers']
     cfg.dataloader.prefetch_factor = 2
     cfg.dataloader.batch_size = config['data']['batch_size']
-    cfg.dataloader.pin_memory = True
+    cfg.dataloader.pin_memory = False
 
     # Training datasets.
     cfg.dataset.modality_config.screen.valid_condition = {"tier": "train"}
     cfg.dataloader.shuffle = True
     train_loader = get_multisession_dataloader(full_paths, cfg)
-
+    print(f'train_loader done, {len(train_loader)}')
     # Validation datasets.
+    cfg.dataloader.batch_size = 4 # for the test mice for video - batch size is often 60, so we need smaller
     cfg.dataset.modality_config.screen.valid_condition = {"tier": "validation"}
+    # this is needed for images
+    for modal in cfg.dataset.modality_config.keys():
+        cfg.dataset.modality_config[modal].chunk_size = cfg.dataset.modality_config[modal].chunk_size // 4
+
+    cfg.dataset.modality_config.screen.sample_stride = cfg.dataset.modality_config.screen.chunk_size 
+
     cfg.dataloader.shuffle = False
     # todo - validation without overlaps
     cfg.dataset.modality_config.screen.sample_stride = (
